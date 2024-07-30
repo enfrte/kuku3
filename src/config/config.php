46e9dfd738183df6988d2d4d3e1d7f2a201dd100
enfrte <leon.forte.fi@gmail.com>
2024-07-30 08:17:11 +0300

    Updated config

--- src/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'app_password' => 'asdf',
-    'database' => [
-        'file_path' => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'database.sqlite'
-    ],
-];
