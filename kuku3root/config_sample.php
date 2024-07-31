83008703124c40e230714ce3722be71983b0c56d
enfrte <leon.forte.fi@gmail.com>
2024-07-31 20:49:22 +0300

    Updates for alwaysdata hosting

---
+++ kuku3root/config_sample.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'app_password' => 'asdf',
+    'database' => [
+        'file_path' => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'database.sqlite'
+    ],
+];
